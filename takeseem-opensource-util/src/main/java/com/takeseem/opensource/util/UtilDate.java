cdcd91f85e87e354f84191478356392afcc296b2
takeseem <takeseem@gmail.com>
2013-06-30 21:07:53 +0800

    构建项目框架

---
+++ takeseem-opensource-util/src/main/java/com/takeseem/opensource/util/UtilDate.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2013-2033 by takeseem.com
+ * All rights reserved.
+ * @author <a href="mailto:takeseem@gmail.com">takeseem.com</a>
+ */
+package com.takeseem.opensource.util;
+
+/**
+ * 日期工具方法
+ */
+public class UtilDate {
+
+}
